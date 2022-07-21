0507a6ef6b046c53595e8dc93e78c3cc69c2006a
Majid <helloabdulmajid>
2022-07-22 01:19:55 +0530

    2nd time commit after .git delete

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import Layout from "./components/pages/Layout";
+import LoginReg from "./components/pages/auth/LoginReg";
+
+
+function App() {
+  return (
+    <>
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<Layout/>} >
+    <Route index element={<Home/>} />
+    <Route path="contact" element={<Contact/>} />
+    <Route path="login" element={<LoginReg/>}/>
+  </Route>
+
+</Routes>
+</BrowserRouter>
+    </>
+  );
+}
+
+export default App;
